64e85b0deea349d4d61890a283cfb063a6408c7a
Jared R Clemence <jaredclemence@pediatricheartcenter.org>
2018-07-05 10:51:39 -0700

    - Created ObjectBuilder - Implemented ObjectBuilder test.

--- src/builders/ObjectBuilder.php
+++ src/builders/ObjectBuilder.php
@@ -8,11 +8,47 @@
 
 namespace JRC\binn\builders;
 
+use JRC\binn\builders\ContainerBuilder;
+use JRC\binn\Size;
+
 /**
  * Description of ObjectBuilder
  *
  * @author jaredclemence
  */
-class ObjectBuilder {
-    //put your code here
+class ObjectBuilder extends ContainerBuilder {
+    protected function addElementAtKey(&$object, $key, $value) {
+        $object->{$key} = $value;
+    }
+
+    protected function createEmptyContainer() {
+        return new \stdClass();
+    }
+
+    /**
+     * Extracts next field identifier from data string from lastPosition.
+     * 
+     * Keys are NOT null terminated. (This is different from STRING containers)
+     * Keys are stored with a key length in one byte stored in front of the key.
+     * 
+     * @param string $data
+     * @param int $lastPosition
+     * @return arary [ $stringKey, $nextPosition ] 
+     */
+    protected function extractKey($data, $lastPosition) {
+        $substring = substr( $data, $lastPosition );
+        $keySizeString = $substring[0];
+        $keySize = $this->convertKeySizeStringToValue( $keySizeString );
+        $keyText = substr( $substring, 1, $keySize );
+        $nextPosition = $lastPosition + 1 /* key length */ + $keySize /* key text length */;
+        return [ $keyText, $nextPosition ];
+    }
+
+    private function convertKeySizeStringToValue($keySizeString) {
+        $keySizeObj = new Size();
+        $keySizeObj->setByteString($keySizeString);
+        $keySize = $keySizeObj->getValue();
+        return $keySize;
+    }
+
 }
---
+++ test/ObjectBuilderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use JRC\binn\builders\ObjectBuilder as Builder;
+
+require_once realpath( __DIR__ . '/../autoload.php' );
+
+/**
+ * Description of ListBuilderTest
+ *
+ * @author jaredclemence
+ */
+class MapBuilderTest extends TestCase {
+    /**
+     * @param type $dataCount
+     * @param type $binaryString
+     * @param type $expectedResult
+     * @dataProvider provideSimpleReadTestCases
+     */
+    public function testSimpleRead( $dataCount, $binaryString, $expectedResult ){
+        //$this->markTestIncomplete();
+        $builder = new Builder();
+        $builder->read( $dataCount, $binaryString );
+        $result = $builder->make();
+        $this->assertTrue( is_object( $result ), "The builder produces an array as output." );
+        foreach( $expectedResult as $key=>$data ){
+            $this->assertObjectHasAttribute( $key, $result, "The result data is missing expected data for the key '$key'." );
+            $this->assertEquals( $data, $result->{$key}, "The result data does not match the expected data." );
+        }
+        foreach( $result as $key=>$data ){
+            $this->assertArrayHasKey( $key, $expectedResult, "The result data has unexpected data for the key '$key'." );
+        }
+    }
+    public function provideSimpleReadTestCases(){
+        return [
+            ["\x00", "", []],
+            ["\x01", "\x01A\x20\x7B", ["A"=>123]],
+            ["\x02", "\x03Cat\x41\xFE\x38\x03Dog\x20\x7B", ["Cat"=>-456, "Dog"=>123]],
+            ["\x03", "\x0Aschnozzles\x20\x7B\x0Bjargonizing\x41\xFE\x38\x05jumpy\x40\x03\x15", ["schnozzles"=>123,"jargonizing"=>-456,"jumpy"=>789]]
+        ];
+    }
+}
